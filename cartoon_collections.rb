3987f80e7a4ef34e293f5324f5f5ef337937474f
Will Hodgson <will.hodgson@current-rms.com>
2019-07-29 12:57:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,11 +18,7 @@ def long_planeteer_calls(array)
   end
 end
 
-def find_the_cheese(value)
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if cheese_types.include?(value)
-    value
-  else
-    nil
-  end
+
 end
