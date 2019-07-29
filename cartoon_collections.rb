4bb78a7940252361e008ce8d8b03485394a5ac57
Will Hodgson <will.hodgson@current-rms.com>
2019-07-29 12:58:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,6 +21,10 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.any? do |element|
-    cheese_types.include?(element)
+    if cheese_types.include?(element)
+      element
+    else
+      nil
+    end
   end
 end
