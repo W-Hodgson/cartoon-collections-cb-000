033e5bbc0934cd9251a24608509e1285d0b4d7cd
Will Hodgson <will.hodgson@current-rms.com>
2019-07-29 12:43:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,11 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  
+  if array.any? {|element| element.length > 4}
+    true
+  else
+    false
+  end
 end
 
 def find_the_cheese# code an argument here
